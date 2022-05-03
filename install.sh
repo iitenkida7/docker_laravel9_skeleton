95604ad07344271a0dec6a1805d5a77b2e45dc01
iitenkida7 <t.ishii@teradox.net>
2022-05-03 11:40:00 +0900

    Modify

--- install.sh
+++ install.sh
@@ -14,8 +14,15 @@ cd ${INSTALL_DIR}
 git clone git@github.com:iitenkida7/docker_laravel9_skeleton.git .
 rm -rf .git
 docker-compose build #--pull --no-cache
+
+# Install Nginx SSL
+SSL_PATH=${INSTALL_DIR}/docker/nginx/ssl
+openssl req -x509 -sha256 -nodes -days 3650 -newkey rsa:2048 -subj /CN=localhost -keyout ${SSL_PATH}/server.key -out ${SSL_PATH}/server.crt
+
+# Install Laravel
 docker-compose run --rm laravel composer create-project laravel/laravel
 mv laravel laravel_ && mv laravel_/laravel . && rmdir laravel_
+
 docker-compose up -d
 
 echo "Let's access!"
