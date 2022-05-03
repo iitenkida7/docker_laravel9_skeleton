7b4a8610a67f6b5642803885f719318eda974ef1
iitenkida7 <t.ishii@teradox.net>
2022-05-03 10:55:50 +0900

    Add install shell

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -ue
+
+CURRENT=$(pwd)
+INSTALL_DIR=${1:-${CURRENT}}
+
+if [ ! -d "${INSTALL_DIR}" ]; then
+  mkdir -p "${INSTALL_DIR}"
+else
+  echo "Error: Already exists directory" 1>&2
+  exit 1;
+fi
+
+cd ${INSTALL_DIR}
+git clone git@github.com:iitenkida7/docker_laravel9_skeleton.git .
+rm -rf .git
+docker-compose build #--pull --no-cache
+docker-compose run --rm laravel composer create-project laravel/laravel
+mv laravel laravel_ && mv laravel_/laravel . && rmdir laravel_
+docker-compose up -d
+
+echo "Let's access!"
+echo "http://localhost/"
